bb5e144282356dc304c2e1f8027a75a87f79002a
katmoran <kmoran@unc.edu>
2024-09-09 19:30:38 -0400

    Add files via upload

---
+++ about_text_files/arthur.py
@@ -0,0 +1,27 @@
+def message ():
+    """Display a simple message"""
+    print( 'I am Arthur')
+    print('King of Britons')
+  
+message ()
+
+def message (king_name):
+    """Display a message with parameter"""
+    print ('I am', king_name)
+    print ('King of the Britons')
+# Call the message function. 
+message('Henry VIII')
+
+def message(king_name, kingdom):
+    """Display positional arguments"""
+    print('I am', king_name)
+    print('King of the', kingdom)
+# Call the message function. 
+message('Lions', 'Mufasa')
+
+def message(king_name, kingdom = 'Egypt'):
+    """Display default value"""
+    print('I am', king_name)
+    print('King of', kingdom)
+# Call the message function. 
+message('Tut', 'Egypt')

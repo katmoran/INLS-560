1849ef117c35cfcf32912bcd371e5630ae0e24a1
katmoran <kmoran@unc.edu>
2024-11-20 20:21:40 -0500

    Add files via upload

--- assignment_08/main.py
+++ assignment_08/main.py
@@ -1,74 +1,73 @@
-import sys # don't use quit() or exit() in a production code. use sys. exit 
+import sys # don't use quit() or exit() in production code. Use sys.exit
 
-# make program repeat: 
-while True: 
+# Make program repeat:
+while True:
 
-# get user input for file:
-    file_variable = input('''
-    What file would you like to search for?:
-    a) 60s music file 
+    # get user input for file:
+    file_variable = input('''What file would you like to search for?:
+    a) 60s-music file
     b) athletes file 
-    x) to exit 
+    x) to exit                                                                  
     ''')
 
-# process user input 
-    if file_variable == 'x':
+
+    # process user input 
+    if file_variable == 'x': 
         sys.exit() # this requires
     elif file_variable == 'a':
         file_variable = '60s-music.csv'
     elif file_variable == 'b':
         file_variable = 'athletes.csv'
-    else: 
-        print('Invalid option. Please select a, b, or x.')
+    else:
+        print('Invalid option. Please select, a, b, or x.')
         continue 
+    # enter a search term this // is a global variable
+    search_variable = input(f'Enter the search term for {file_variable} file:')
+    search_variable = search_variable.title() # Make it so that the user can enter lower case term
 
-    # enter a search term this // is a global website
-    search_variable = input(f'Enter the search term for {file_variable} file: ')
-    search_variable = search_variable.title() # Make it so that the user can enter lower-case term. 
 
-
-    # go to 02_search_for_term.py to continue  .....
-    def find(file_variable, search_variable): 
+    # go to 02_search_for_term.py to continue......
+    def find(file_variable, search_variable):
         with open(file_variable, 'r') as file:
             content = file.read()
-    # now the file is in the memory buffer as content 
+    # now check to the file is in hte memory buffer as content 
 
-    # Next check to see if the search_variable is in the content buffer: 
-        if search_variable in content: 
-
-        # if the file print that it is in the file AND 
-        # if the user wants to see the entries for the term
-            print(f'Your search term {search_variable} exists in the {file_variable} file!')
-            see_entries = input('Would you like to see the entries? (y or n)?')
-
-        # if y then run this code to output all the entries: 
-        if see_entries.lower() == 'y':
-            print(f'Here are all the of the entries with the term {search_variable}:')
-            with open(file_variable, 'r') as file:
-                for line in file: 
-                    if search_variable in line: # 
-                        print(line.strip()) 
-
-
-        # if N lowercase (user does not want to) then run this code: 
-        elif see_entries.lower() == 'n':
-            print('Goodbye')
-            sys.exit()  
+    # next check to see if the search_variable is in the content buffer: 
+            if search_variable in content: 
             
+            # if the file print that it is in the file AND 
+            # if user wants to see the entries for teh term 
+                print(f'Your search term {search_variable} exits in the {file_variable} file!')
+                see_entries = input('Would you like to see the entries? (y or n)?')
 
-        else: 
-            print("Invalid option. Please enter y or n.")
+                # if y then run this code to output all the entries: 
+                if see_entries.lower() == 'y':
+                    print(f'Here are all of the entries with the term {search_variable}:')
+                    with open(file_variable, 'r') as file:
+                        for line in file:
+                            if search_variable in line: # 
+                                    print(line.strip())
 
 
-    # if it is not, print that it is there
-    else:
-        print(f'{search_variable} does not exist in {file_variable}')
-        
-# call the function 
-find(file_variable, search_variable)
+
+                #if N lowercase (user does not want to ) then run this code: 
+                elif see_entries.lower() == 'n':
+                    print('Goodbye')
+                    sys.exit()
+
+                else:
+                    print(f"Invalid option. Please enter y or n.")
 
 
-    
-        
+            # if it is not, print that it is there
+            else: 
+                print(f'{search_variable} does not exist in {file_variable}')
 
-        
+
+    # call the function 
+    find(file_variable, search_variable)    
+
+
+
+                                 
+
